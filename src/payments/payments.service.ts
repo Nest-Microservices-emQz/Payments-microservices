3eedec17118e1fc26ac9ec53b173fc5f04530a88
Enrique Marquez <enrique.marquezp@gmail.com>
2024-06-19 07:30:23 -0700

    First Commit

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class PaymentsService {
+
+    private readonly stripe = new Stripe(envs.stripeSecret)
+
+    async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+
+        const { currency, items, orderId } = paymentSessionDto;
+
+        const lineItems = items.map(item => {
+            return {
+                price_data: {
+                    currency: currency,
+                    product_data: {
+                        name: item.name
+                    },
+                    unit_amount: Math.round(item.price * 100),
+                },
+                quantity: item.quantity
+            }
+        })
+
+        const session = await this.stripe.checkout.sessions.create({
+            // Colocar aquí el ID de mi orden
+            payment_intent_data: {
+                metadata: { 
+                    orderId: orderId
+                }
+            },
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: 'http://localhost:3003/payments/success',
+            cancel_url: 'http://localhost:3003/payments/cancel'
+        });
+
+        return session
+
+    }
+
+    async stripeWebhok(req: Request, res: Response){
+        const sig = req.headers['stripe-signature'];
+        
+        let event: Stripe.Event;
+        
+        //Testing EndPoint
+        // const endpointSecret = "whsec_183c9024ad733607f61e781b41f31706e0ea78ee879b8121f2dc07a2eb0417fc";
+        
+        // Real Endpoint
+        const endpointSecret = "whsec_IZ2cLuXkGlp3TkvtRFdmKR8eRQ1fUv6G";
+
+        try {
+            event = this.stripe.webhooks.constructEvent(req['rawBody'], sig, endpointSecret);
+        } catch (err) {
+            res.status(400).send(`Webhook Error: ${err.message}`);
+        }
+
+        switch( event.type ){
+            case 'charge.succeeded':
+                const chargeSucceeded = event.data.object;
+                console.log({
+                    metadata: chargeSucceeded.metadata,
+                    orderId: chargeSucceeded.metadata.orderId
+                })
+            break;
+
+            default: 
+                console.log(`Event ${event.type} not handled`)
+        }
+
+        return res.status(200).json( { sig } )
+    }
+}
